51c31bcd420fc4b9bc165ba3bcb32c1d6011df4d
Mohammed Tawfeq Amiri <mahmmedtawfeqamirie@gmail.com>
2022-12-06 16:47:50 +0300

    initial pages home and products

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'products',
+    loadChildren: () => import('./modules/item/item.module').then(m => m.ItemModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
     initialNavigation: 'enabledBlocking'
-})],
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
