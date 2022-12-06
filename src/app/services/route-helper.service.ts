f68c3b161e2e46d3960ffd68315961a839dc3bc8
Mohammed Tawfeq Amiri <mahmmedtawfeqamirie@gmail.com>
2022-12-06 19:01:22 +0300

    social media as services seo per page

---
+++ src/app/services/route-helper.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
+import { SeoSocialShareService } from 'ngx-seo';
+import { filter, map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RouteHelper {
+
+  constructor(
+    private readonly router: Router,
+    private readonly activatedRoute: ActivatedRoute,
+    private readonly seoSocialShareService: SeoSocialShareService
+  ) {
+    this.setupRouting();
+  }
+
+  private setupRouting() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(() => this.activatedRoute),
+      map(route => {
+        while (route.firstChild) {
+          route = route.firstChild;
+        }
+        return route;
+      }),
+      filter(route => route.outlet === 'primary')
+    ).subscribe((route: ActivatedRoute) => {
+      const seo = route.snapshot.data['seo'];
+      // set your meta tags & title here
+      this.seoSocialShareService.setData(seo);
+    });
+  }
+
+}
