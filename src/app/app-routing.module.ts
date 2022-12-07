c134a8a5b07adb762b96b87265d284f6b5d25156
Mohammed Tawfeq Amiri <mahmmedtawfeqamirie@gmail.com>
2022-12-07 18:09:39 +0300

    SEO ON APP

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SEOHomePage, SEOProductPage } from './models/seo-pages';
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    redirectTo: 'home'
+    redirectTo: 'screen'
   },
   {
-    path: 'home',
+    path: 'screen',
+    data: {
+      seo: SEOHomePage
+    },
     loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)
   },
   {
     path: 'products',
+    data: {
+      seo: SEOProductPage
+    },
     loadChildren: () => import('./modules/item/item.module').then(m => m.ItemModule)
   }
 ];
--- src/app/modules/item/item-routing.module.ts
+++ src/app/modules/item/item-routing.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SEOProductPage } from 'src/app/models/seo-pages';
 import { ItemComponent } from './item.component';
 
-const routes: Routes = [{ path: '', data: { seo: SEOProductPage }, component: ItemComponent }];
+const routes: Routes = [{ path: '', component: ItemComponent }];
 
 
 @NgModule({
