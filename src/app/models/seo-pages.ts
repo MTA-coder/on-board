e9e3370f424ca9216ecc26faa7f84b7265ac734a
Mohammed Tawfeq Amiri <mahmmedtawfeqamirie@gmail.com>
2022-12-07 16:12:27 +0300

    DEv SSR

--- src/app/models/seo-pages.ts
+++ src/app/models/seo-pages.ts
@@ -1,7 +1,7 @@
 import { SeoSocialShareData } from 'ngx-seo';
 
 export const SEOHomePage: SeoSocialShareData = {
-  title: 'On-Board Angular Home Screen',
+  title: 'On-Board Angular Home Screen a',
   type: 'website',
   image: 'https://cdn.educba.com/academy/wp-content/uploads/2022/05/Angular-Material-Design.jpg',
   description: 'How to build  application with angular ssr and social media preview and multi languages',
