808539f2726905cea0c7478383ef535da3f025b2
Mohammed Tawfeq Amiri <mahmmedtawfeqamirie@gmail.com>
2022-12-07 14:38:15 +0300

    SEO On Components

--- src/app/models/seo-pages.ts
+++ src/app/models/seo-pages.ts
@@ -6,7 +6,7 @@ export const SEOHomePage: SeoSocialShareData = {
   image: 'https://cdn.educba.com/academy/wp-content/uploads/2022/05/Angular-Material-Design.jpg',
   description: 'How to build  application with angular ssr and social media preview and multi languages',
   author: '@mta',
-  url: 'https://mta-coder.github.io/on-board/home/'
+  url: 'https://mta-coder.github.io/on-board/home'
 };
 
 export const SEOProductPage: SeoSocialShareData = {
@@ -15,5 +15,5 @@ export const SEOProductPage: SeoSocialShareData = {
   image: 'https://images.indepth.dev/images/2021/05/Angular-Forms_-reactive-design-patterns-catalog2.jpg',
   description: 'How to build  application with angular ssr and social media preview and multi languages',
   author: '@mta',
-  url: 'https://mta-coder.github.io/on-board/products/'
+  url: 'https://mta-coder.github.io/on-board/products'
 };
